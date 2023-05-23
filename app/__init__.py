2f2a323fdfeed862d5cc33b7aa5cefca412baf0a
daveleone <it.davide.leone@gmail.com>
2023-05-23 21:13:11 +0200

    modified init and created a blank database file

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from database import db
 
 # Create the Flask application object
 app = Flask(__name__)
+app.config.from_object('config.Config')
+db.init_app(app)
 
 # Set the configuration options
-#app.config.from_object('config')
+app.config.from_object('config')
 
 # Import the views module to register the routes
 from app import routes
